65e7ed10623a148cffa2ff90220a4f231f7c686e
Jo Van Eyck <jo.vaneyck@ae.be>
2019-11-03 16:11:58 +0100

    inlined generators

--- csharp/GoldenMasterTest.cs
+++ csharp/GoldenMasterTest.cs
@@ -9,17 +9,12 @@ namespace csharp
         [Fact]
         public void Run()
         {
-            var nameGenerator = Gen.Elements("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "my magical item");
-            var qualityGenerator = Gen.Elements(-1, 0, 1, 49, 50, 51);
-            var sellInGenerator = Gen.Elements(-1, 0, 1, 5, 6, 7, 10, 11, 12);
-
-            var itemGenerator =
-                from name in nameGenerator
-                from quality in qualityGenerator
-                from sellIn in sellInGenerator
-                select new Item {Name = name, Quality = quality, SellIn = sellIn};
-
-            var itemsGenerator = Gen.ListOf(itemGenerator);
+            var itemsGenerator = 
+                Gen.ListOf(
+                    from name in Gen.Elements("Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "my magical item")
+                    from quality in Gen.Elements(-1, 0, 1, 49, 50, 51)
+                    from sellIn in Gen.Elements(-1, 0, 1, 5, 6, 7, 10, 11, 12)
+                    select new Item {Name = name, Quality = quality, SellIn = sellIn});
 
             var items = itemsGenerator
                 .Eval(1000, Random.mkStdGen(1337))
