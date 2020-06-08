18b9015336562c682e7e9b332080cb5043d55e8a
Van Eyck Jo <jo.vaneyck@fluxys.com>
2020-06-08 11:30:30 +0200

    [csharpcore] add default unnamed item to cover all code paths

--- csharpcore/ApprovalTest.cs
+++ csharpcore/ApprovalTest.cs
@@ -28,13 +28,9 @@ namespace csharpcore
         [Fact]
         public void AllCombinations()
         {
-            var items = new List<Item>();
-
-            
-
             ApprovalTests.Combinations.CombinationApprovals.VerifyAllCombinations(
                 RunItem,
-                new [] { "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" },
+                new [] { "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "default item" },
                 new[] { -1, 0, 1, 49, 50, 51 },
                 new [] { -1, 0, 1, 5, 6, 7, 10, 11, 12 });
         }
