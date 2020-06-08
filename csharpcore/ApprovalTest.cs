f175d5de3026e98588897fda1e8e74ebdd772828
Van Eyck Jo <jo.vaneyck@fluxys.com>
2020-06-08 11:27:14 +0200

    [csharpcore] added combinationapprovals

--- csharpcore/ApprovalTest.cs
+++ csharpcore/ApprovalTest.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using ApprovalTests;
@@ -22,6 +23,30 @@ namespace csharpcore
             var output = fakeoutput.ToString();
 
             Approvals.Verify(output);
+        }
+
+        [Fact]
+        public void AllCombinations()
+        {
+            var items = new List<Item>();
+
+            
+
+            ApprovalTests.Combinations.CombinationApprovals.VerifyAllCombinations(
+                RunItem,
+                new [] { "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" },
+                new[] { -1, 0, 1, 49, 50, 51 },
+                new [] { -1, 0, 1, 5, 6, 7, 10, 11, 12 });
+        }
+
+        private string RunItem(string name, int quality, int sellIn)
+        {
+            var item = new Item{Name = name, Quality = quality, SellIn = sellIn};
+            
+            var app = new GildedRose(new List<Item> {item});
+            app.UpdateQuality();
+
+            return $"[{item.Name},{item.Quality},{item.SellIn}]";
         }
     }
 }
