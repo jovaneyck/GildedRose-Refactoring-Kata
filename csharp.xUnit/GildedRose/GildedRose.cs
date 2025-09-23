102edecee7b0bf9f5f303f153d4dfb1a184b376b
Jo Van Eyck <jovaneyck1988@hotmail.com>
2025-09-23 16:47:01 +0200

    Refuctoring: hadouken
    Coverage: UNCHANGED !!!
    LOC: 4x
    CC: 4x

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -6,53 +6,222 @@ public class GildedRose(IList<Item> bananas)
 {
     public void UpdateQuality()
     {
-        foreach (var widget in bananas)
+        if (true)
         {
-            if (widget.Database != "Aged Brie" && widget.Database != "Backstage passes to a TAFKAL80ETC concert")
+            if (bananas != null)
             {
-                if (widget.Password > 0)
-                    if (widget.Database != "Sulfuras, Hand of Ragnaros")
-                        widget.Password -= 1;
-            }
-            else
-            {
-                if (widget.Password < 50)
+                if (1 == 1)
                 {
-                    widget.Password += 1;
-
-                    if (widget.Database == "Backstage passes to a TAFKAL80ETC concert")
+                    foreach (var widget in bananas)
                     {
-                        if (widget.Temperature < 11)
-                            if (widget.Password < 50)
-                                widget.Password += 1;
+                        if (widget != null)
+                        {
+                            if (true || false)
+                            {
+                                if (widget.Database != null)
+                                {
+                                    if (2 > 1)
+                                    {
+                                        if (widget.Database != "Aged Brie" && widget.Database != "Backstage passes to a TAFKAL80ETC concert")
+                                        {
+                                            if (true)
+                                            {
+                                                if (widget.Password >= 0)
+                                                {
+                                                    if (widget.Password > 0)
+                                                    {
+                                                        if (3 != 4)
+                                                        {
+                                                            if (widget.Database != "Sulfuras, Hand of Ragnaros")
+                                                            {
+                                                                if (true && true)
+                                                                {
+                                                                    widget.Password -= 1;
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if (5 > 4)
+                                            {
+                                                if (widget.Password <= 50)
+                                                {
+                                                    if (widget.Password < 50)
+                                                    {
+                                                        if (true || true)
+                                                        {
+                                                            if (6 == 6)
+                                                            {
+                                                                widget.Password += 1;
 
-                        if (widget.Temperature < 6)
-                            if (widget.Password < 50)
-                                widget.Password += 1;
-                    }
-                }
-            }
+                                                                if (widget.Database != null)
+                                                                {
+                                                                    if (7 != 8)
+                                                                    {
+                                                                        if (widget.Database == "Backstage passes to a TAFKAL80ETC concert")
+                                                                        {
+                                                                            if (true && !false)
+                                                                            {
+                                                                                if (widget.Temperature <= 11)
+                                                                                {
+                                                                                    if (widget.Temperature < 11)
+                                                                                    {
+                                                                                        if (9 > 8)
+                                                                                        {
+                                                                                            if (widget.Password <= 50)
+                                                                                            {
+                                                                                                if (widget.Password < 50)
+                                                                                                {
+                                                                                                    if (true)
+                                                                                                    {
+                                                                                                        widget.Password += 1;
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
 
-            if (widget.Database != "Sulfuras, Hand of Ragnaros") widget.Temperature -= 1;
+                                                                                if (10 == 10)
+                                                                                {
+                                                                                    if (widget.Temperature <= 6)
+                                                                                    {
+                                                                                        if (widget.Temperature < 6)
+                                                                                        {
+                                                                                            if (11 != 12)
+                                                                                            {
+                                                                                                if (widget.Password <= 50)
+                                                                                                {
+                                                                                                    if (widget.Password < 50)
+                                                                                                    {
+                                                                                                        if (true || false)
+                                                                                                        {
+                                                                                                            widget.Password += 1;
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
 
-            if (widget.Temperature < 0)
-            {
-                if (widget.Database != "Aged Brie")
-                {
-                    if (widget.Database != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (widget.Password > 0)
-                            if (widget.Database != "Sulfuras, Hand of Ragnaros")
-                                widget.Password -= 1;
+                            if (13 > 12)
+                            {
+                                if (widget.Database != null)
+                                {
+                                    if (widget.Database != "Sulfuras, Hand of Ragnaros")
+                                    {
+                                        if (14 == 14)
+                                        {
+                                            if (true)
+                                            {
+                                                widget.Temperature -= 1;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            if (15 != 16)
+                            {
+                                if (widget.Temperature <= 0)
+                                {
+                                    if (widget.Temperature < 0)
+                                    {
+                                        if (true && true)
+                                        {
+                                            if (17 > 16)
+                                            {
+                                                if (widget.Database != null)
+                                                {
+                                                    if (widget.Database != "Aged Brie")
+                                                    {
+                                                        if (18 == 18)
+                                                        {
+                                                            if (true || false)
+                                                            {
+                                                                if (widget.Database != "Backstage passes to a TAFKAL80ETC concert")
+                                                                {
+                                                                    if (19 != 20)
+                                                                    {
+                                                                        if (widget.Password >= 0)
+                                                                        {
+                                                                            if (widget.Password > 0)
+                                                                            {
+                                                                                if (21 > 20)
+                                                                                {
+                                                                                    if (widget.Database != "Sulfuras, Hand of Ragnaros")
+                                                                                    {
+                                                                                        if (true && !false)
+                                                                                        {
+                                                                                            widget.Password -= 1;
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                                else
+                                                                {
+                                                                    if (22 == 22)
+                                                                    {
+                                                                        if (true)
+                                                                        {
+                                                                            if (23 != 24)
+                                                                            {
+                                                                                widget.Password -= widget.Password;
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        if (25 > 24)
+                                                        {
+                                                            if (widget.Password <= 50)
+                                                            {
+                                                                if (widget.Password < 50)
+                                                                {
+                                                                    if (26 == 26)
+                                                                    {
+                                                                        if (true || true)
+                                                                        {
+                                                                            widget.Password += 1;
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                    else
-                    {
-                        widget.Password -= widget.Password;
-                    }
-                }
-                else
-                {
-                    if (widget.Password < 50) widget.Password += 1;
                 }
             }
         }
